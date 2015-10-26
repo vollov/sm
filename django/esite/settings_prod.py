d9980400bf29f7a76ec4f9693b76808f7f2a3359
dustin.zhang <vollov.ca@gmail.com>
2015-10-25 21:51:43 -0400

    prod settings

--- django/esite/settings_prod.py
+++ django/esite/settings_prod.py
@@ -1,7 +1,7 @@
 from settings import *
 
 DEBUG = TEMPLATE_DEBUG = CAPTCHA_TEST_MODE = False
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.nthomeservice.ca', '.freeindex.ca']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.jennyli.ca']
 
 # the number of seconds the cookie can live for
 #SESSION_COOKIE_AGE=1209600
