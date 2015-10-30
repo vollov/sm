b0c018338c9ee38bfc9920f6a1f9a59563627f6d
Dustin Zhang <dike.zhang@gmail.com>
2015-10-30 16:33:18 -0400

    clean i18n

--- django/store/views.py
+++ django/store/views.py
@@ -57,6 +57,7 @@ from django.contrib.auth import logout
 from django.http import HttpResponseRedirect
 
 def logout_view(request):
+    logger.debug('calling store.views.logout_view()')
     logout(request)
     return HttpResponseRedirect('/')
 
